5e303b7c6a6e7de43ab73fc84b1abcd530458160
hungchun-lin <49414620+hungchun-lin@users.noreply.github.com>
2020-09-27 15:52:11 -0400

    Create Week2_0922.mk

---
+++ Meeting Note/Week2_0922.mk
@@ -0,0 +1,21 @@
+Week2 - 0922 Meeting note:
+
+1. What is the detail of the struction, what will be input in LSTM, what will be output?
+  a. Input data
+    Stock price data: Apple real stock price
+    Features: Stock Index
+              Economic Index
+              Technical Indicator
+              Fourier Transform
+              Daily News
+  b. LSTM (Stacked LSTM)
+![alt text](http://url/to/img.png)
+  
+       
+
+
+2. Contribution -> Use different GAN
+                -> Baseline: Stanford report?
+                
+               
+3. Github: Folder -> Paper/ Code/ Meeting note
