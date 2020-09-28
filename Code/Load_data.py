8d83121af26c9c80324df3940cd67fb14f517235
hungchun-lin <49414620+hungchun-lin@users.noreply.github.com>
2020-09-27 20:55:11 -0400

    Add files via upload

---
+++ Code/Load_data.py
@@ -0,0 +1,90 @@
+import os
+import numpy as np
+import csv
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.dates import DateFormatter
+import math
+# with open('DATA.csv', 'r') as file:
+#     reader = csv.reader(file)
+#     names = next(reader)
+#     print(names)
+#     for row in reader:
+#         print(row)
+
+## import data
+df = pd.read_csv('DATA.csv', parse_dates=['Date'])
+print(df.head())
+print(df.tail())
+print(df.shape)
+print(df.columns)
+
+# Create Apple stock price plot
+## https://www.earthdatascience.org/courses/use-data-open-source-python/use-time-series-data-in-python/date-time-types-in-pandas-python/customize-dates-matplotlib-plots-python/
+fig, ax = plt.subplots(figsize=(10,3))
+ax.plot(df['Date'], df['APPLE Close'], label='Apple stock')
+ax.set(xlabel="Date",
+       ylabel="USD",
+       title="Apple Stock Price")
+date_form = DateFormatter("%Y")
+ax.xaxis.set_major_formatter(date_form)
+plt.show()
+
+# Calculate technical indicators
+def get_technical_indicators(data):
+    # Create 7 and 21 days Moving Average
+    data['MA7'] = data.iloc[:,1].rolling(window=7).mean()
+    data['MA21'] = data.iloc[:,1].rolling(window=21).mean()
+
+    # Create MACD
+    data['MACD'] = data.iloc[:,1].ewm(span=26).mean() - data.iloc[:,1].ewm(span=12,adjust=False).mean()
+
+    # Create Bollinger Bands
+    data['20SD'] = data.iloc[:, 1].rolling(20).std()
+    data['upper_band'] = data['MA21'] + (data['20SD'] * 2)
+    data['lower_band'] = data['MA21'] - (data['20SD'] * 2)
+
+    # Create Exponential moving average
+    data['EMA'] = data.iloc[:,1].ewm(com=0.5).mean()
+
+    # Create LogMomentum
+    data['logmomentum'] = np.log(data.iloc[:,1] - 1)
+
+    return data
+T_df = get_technical_indicators(df)
+print(T_df.head())
+
+
+def plot_technical_indicators(dataset, last_days):
+       plt.figure(figsize=(16, 10), dpi=100)
+       shape_0 = dataset.shape[0]
+       xmacd_ = shape_0 - last_days
+
+       dataset = dataset.iloc[-last_days:, :]
+       x_ = range(3, dataset.shape[0])
+       x_ = list(dataset.index)
+
+       # Plot first subplot
+       plt.subplot(2, 1, 1)
+       plt.plot(dataset['MA7'], label='MA 7', color='g', linestyle='--')
+       plt.plot(dataset['APPLE Close'], label='Closing Price', color='b')
+       plt.plot(dataset['MA21'], label='MA 21', color='r', linestyle='--')
+       plt.plot(dataset['upper_band'], label='Upper Band', color='c')
+       plt.plot(dataset['lower_band'], label='Lower Band', color='c')
+       plt.fill_between(x_, dataset['lower_band'], dataset['upper_band'], alpha=0.35)
+       plt.title('Technical indicators for Goldman Sachs - last {} days.'.format(last_days))
+       plt.ylabel('USD')
+       plt.legend()
+
+       # Plot second subplot
+       plt.subplot(2, 1, 2)
+       plt.title('MACD')
+       plt.plot(dataset['MACD'], label='MACD', linestyle='-.')
+       plt.hlines(15, xmacd_, shape_0, colors='g', linestyles='--')
+       plt.hlines(-15, xmacd_, shape_0, colors='g', linestyles='--')
+       plt.plot(dataset['logmomentum'], label='Momentum', color='b', linestyle='-')
+
+       plt.legend()
+       plt.show()
+
+plot_technical_indicators(T_df, 400)
