f8ecef4bf50dddb783dca385d8a04ea9e94df6da
ChenChen <46377474+ChenChenDS@users.noreply.github.com>
2020-11-30 13:54:44 -0500

    Update 2. data_preprocessing.py

--- Code/2. data_preprocessing.py
+++ Code/2. data_preprocessing.py
@@ -1,43 +1,17 @@
-import time
 import os
 import pandas as pd
-import tensorflow as tf
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-import tensorflow
-#import statsmodels.api as sm
+import statsmodels.api as sm
 from numpy import *
 from math import sqrt
 from pandas import *
 from datetime import datetime, timedelta
-
-from sklearn.preprocessing import LabelEncoder, MinMaxScaler
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
 from pickle import dump
 
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout
-from tensorflow.keras.layers import BatchNormalization, Embedding, TimeDistributed, LeakyReLU
-from tensorflow.keras.layers import LSTM
-from tensorflow.keras.optimizers import Adam
-
-from matplotlib import pyplot
-
 # %% --------------------------------------- Load Data  -----------------------------------------------------------------
-
-# If using VAE features
-# Run this
-
-#Dataset_I = pd.read_csv('Finaldata_with_Fourier.csv', parse_dates=['Date'])
-#dataset = pd.read_csv('Finaldata_with_Fourier.csv', parse_dates=['Date'])
-#Dataset_II = pd.read_csv('VAE_fearues.csv')
-#dataset = pd.concat([Dataset_I, Dataset_II], axis=1)
-
-# If NOT using VAE features
-# Run this
 dataset = pd.read_csv('Finaldata_with_Fourier.csv', parse_dates=['Date'])
 news = pd.read_csv("News.csv", parse_dates=["Date"])
 
@@ -66,8 +40,8 @@ X_value = pd.DataFrame(dataset.iloc[:, :])
 y_value = pd.DataFrame(dataset.iloc[:, 3])
 
 # Autocorrelation Check
-# sm.graphics.tsa.plot_acf(y_value.squeeze(), lags=100)
-# plt.show()
+sm.graphics.tsa.plot_acf(y_value.squeeze(), lags=100)
+plt.show()
 
 # Normalized the data
 X_scaler = MinMaxScaler(feature_range=(-1, 1))
@@ -90,28 +64,11 @@ n_features = X_value.shape[1]
 n_steps_out = 1
 
 # Get X/y dataset
-# def get_X_y(X_data, y_data):
-#     X = list()
-#     y = list()
-#     yc = list()
-#
-#     for i in range(len(X_data) - n_steps_in):
-#         X_value = X_data[i: i + n_steps_in][:, :]
-#         y_value = y_data[i + n_steps_in + 1: i + n_steps_in + n_steps_out + 1, 0]
-#         print(y_value)
-#         print(y_value.shape)
-#         yc_value = y_data[i: i + n_steps_in][:, :]
-#         X.append(X_value)
-#         y.append(y_value)
-#         yc.append(yc_value)
-#
-#     return np.array(X), np.array(y), np.array(yc)
 def get_X_y(X_data, y_data):
     X = list()
     y = list()
     yc = list()
 
-    #length = len(X_data) - n_steps_in - n_steps_out + 1
     length = len(X_data)
     for i in range(0, length, 1):
         X_value = X_data[i: i + n_steps_in][:, :]
@@ -141,37 +98,12 @@ def split_train_test(data):
     return data_train, data_test
 
 # Get data and check shape
-
 X, y, yc = get_X_y(X_scale_dataset, y_scale_dataset)
-
-#y = y.reshape(y.shape[0], 1)
 X_train, X_test, = split_train_test(X)
 y_train, y_test, = split_train_test(y)
 yc_train, yc_test, = split_train_test(yc)
 index_train, index_test, = predict_index(dataset, X_train, n_steps_in, n_steps_out)
-
-#y = y.reshape(y.shape[0], 1)
-#y = y.reshape(y.shape[0], y.shape[1], 1)
-# y_train = y_train.reshape(y_train.shape[0], 1)
-# y_test = y_test.reshape(y_test.shape[0], 1)
-# yc_test = yc_test.reshape(yc_test.shape[0], 1)
-# yc_train = yc_train.reshape(yc_train.shape[0], 1)
 # %% --------------------------------------- Save dataset -----------------------------------------------------------------
-
-# get the train test predict index
-# def predict_index(dataset, X_train, n_steps_in, n_steps_out):
-#
-#     # get the predict data (remove the in_steps days)
-#     train_predict_index = dataset.iloc[n_steps_in:X_train.shape[0]+n_steps_in + n_steps_out - 1, :].index
-#     test_predict_index = dataset.iloc[X_train.shape[0]+n_steps_in:, :].index
-#
-#     return train_predict_index, test_predict_index
-# train_predict_index, test_predict_index = predict_index(dataset, X_train, n_steps_in, n_steps_out)
-
-
-# print('dataset_train', dataset_train)
-# print('dataset_test', dataset_test)
-
 print('X shape: ', X.shape)
 print('y shape: ', y.shape)
 print('X_train shape: ', X_train.shape)
@@ -182,8 +114,6 @@ print('y_test shape: ', y_test.shape)
 print('y_c_test shape: ', yc_test.shape)
 print('index_train shape:', index_train.shape)
 print('index_test shape:', index_test.shape)
-#print('train_predict_index: ', train_predict_index.shape)
-#print('test_predict_index: ', test_predict_index.shape)
 
 np.save("X_train.npy", X_train)
 np.save("y_train.npy", y_train)
@@ -193,5 +123,4 @@ np.save("yc_train.npy", yc_train)
 np.save("yc_test.npy", yc_test)
 np.save('index_train.npy', index_train)
 np.save('index_test.npy', index_test)
-#np.save("train_predict_index.npy", train_predict_index)
-#np.save("test_predict_index.npy", test_predict_index)
+
